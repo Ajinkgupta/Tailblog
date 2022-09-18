7772307b13a82dc367a27a8bcea305dedfe2d736
Ajinkgupta <76146259+Ajinkgupta@users.noreply.github.com>
2022-09-19 00:02:00 +0530

    Update head_section.php

--- includes/head_section.php
+++ includes/head_section.php
@@ -3,6 +3,7 @@
 
 <head>
   <meta charset="UTF-8">
+  <base href="<?php echo BASE_URL ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://cdn.tailwindcss.com"></script>
   <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
